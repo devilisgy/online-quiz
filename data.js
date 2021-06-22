d8e7e678a74ffe3207f469e177c4130f9c91161c
Yash <marcosdevil123@gmail.com>
2021-06-22 13:03:00 +0530

    First Commit

---
+++ data.js
@@ -0,0 +1,41 @@
+var jsonData = [
+    {
+        "q" : "1. Which nation has objected the inclusion of Kalapani in the new political map of India?",
+        "opt1" :"Nepal",
+        "opt2" :"China",
+        "opt3" :"Srilanka",
+        "answer" :"Nepal"
+
+    },
+    {
+        "q" : "Which two state assemblies came together for a special assembly session on November 6?",
+        "opt1" :"J&K and Uttarakhand",
+        "opt2" :"Haryana and Punjab",
+        "opt3" :"Rajasthan and Gujarat",
+        "answer" :"Haryana and Punjab"
+    },
+    {
+        "q" : "Where was the 5th Edition of â€˜Miss Asia Global Title 2019â€™ organized?",
+        "opt1" :"Delhi",
+        "opt2" :"Kerala",
+        "opt3" :"Maharashtra",
+        "answer" :"Kerala"
+
+    },
+    {
+        "q" : "India's first-ever helicopter summit held in which Indian state?",
+        "opt1" :"Dehradun",
+        "opt2" :"Ranchi",
+        "opt3" :"Lucknow",
+        "answer" :"Dehradun"
+
+    },
+    {
+        "q" : " Prime Minister Narendra Modi inaugurates Indiaâ€™s second Multi-Modal Terminal at Sahibganj in which state",
+        "opt1" :"Assam",
+        "opt2" :"Jharkhand",
+        "opt3" :"Bihar",
+        "answer" :"Jharkhand"
+
+    }
+];
